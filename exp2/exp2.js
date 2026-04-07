2faa48368e6d3ef137a4be8915de3d6330cb097b
Amith V <117374352+Amith0707@users.noreply.github.com>
2026-04-07 07:04:54 +0530

    Update exp2.js

--- exp2/exp2.js
+++ exp2/exp2.js
@@ -1,74 +1,55 @@
-// const express=require('express');
-// const cors=require('cors');
-// const mongoose=require('mongoose');
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
 const app=express();
 const PORT=5000;
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-/*
-MongoDB Connection
-*/
-
-mongoose.connect('mongodb://localhost:27017/Test1')
+// Connects our application to mongodb database named test1
+mongoose.connect('mongodb://localhost:27017/Test1') 
+// So this is promise handling
     .then(()=>{
-        console.log('MongoDB Connected');
+        console.log('MongoDB COnnected');
     })
     .catch((err)=>{
-        console.error("Mongodb connection error.",err);
+        console.error('Failed to connect.',err);
     });
 
-/*
-User schema and model
-*/
-
+// Schema and model (CORE Concept)
 const UserSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    }
-},{timestamps:true});
+    name:{type:String,required:true},
+    email:{type:String,required:true}
+},{timestamps:true});//automatically adds createdAt and updatedAt
 
 const User=mongoose.model('User',UserSchema);
-
-//adding routes
+// Creates a model which is used to interact with the database collection.
 
 app.get('/',(req,res)=>{
-    res.send("API is working");
+    res.send('API is working');
 });
 
 app.get('/users',async(req,res)=>{
     try{
-        const users=await User.find();
-        res.json(users);
+        const user=await User.find(); //user.find() fetches all users from mongodbs
+        res.json(user);
     }catch(err){
         res.status(500).json({message:err.message});
     }
 });
 
-//get user by id
 app.get('/users/:id',async(req,res)=>{
     try{
         const user=await User.findById(req.params.id);
-        if (!user){
-            return res.status(404).json({message:'User not found'});
+        if(!user){
+            res.status(404).json({message:'User not found'})
         }
-        res.json(user);
     }catch(err){
         res.status(400).json({message:'INVALID ID'});
     }
 });
 
-//create user
 app.post('/users',async(req,res)=>{
     try{
         const newUser=new User({
@@ -76,13 +57,49 @@ app.post('/users',async(req,res)=>{
             email:req.body.email
         });
         const savedUser=await newUser.save();
-        res.status(201).json(savedUser);
+        res.status(201).json({message:"Craetd New user succesfully",newUser});
     }catch(err){
         res.status(500).json({message:err.message});
     }
 });
 
+app.delete('/users', async (req, res) => {
+    try {
+        await User.deleteMany();
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
+        res.json({ message: "All users deleted" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.delete('/users/:id',async(req,res)=>{
+    try{
+        const deletedUser=await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User deleted successfully" });        
+    }catch(err){
+        res.status(400).json({message:"INVALID ID"});
+    }
+});
+
+app.put('/users/:id',async(req,res)=>{
+    try{
+        const updatedUser=await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                name:req.body.email,
+                email:req.body.email
+            },
+            {new:true}
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: "Invalid ID" });
+    }
 });
